6136e932b8a6be032dad68fa8014ba0e7bc4f7db
Fagner Pimentel <fagnerpimentel@gmail.com>
2026-09-21 16:27:05 -0300

    Exemplos da aula 4

---
+++ Aula_5/script.js
@@ -0,0 +1,38 @@
+// alert("Olá");
+// console.log("Oi");
+
+// x = prompt("Informe o seu nome");
+// console.log("Seu nome é " + x);
+// alert("Seu nome é " + x);
+
+
+console.log("Exercício 1")
+
+// i = 1;
+// while (i <= 100) {
+//     console.log(i);
+//     i += 2;
+// }
+
+// i = 0;
+// while (i <= 100) {
+//     if(i%2){
+//         console.log(i);
+//     }
+//     i++;
+// }
+
+for(i = 1; i<=100; i+=2){
+    console.log(i)
+}
+
+console.log("Exercício 2")
+for(i = 5; i<=500; i+=5){
+    console.log(i)
+}
+
+console.log("Exercício 3")
+x = prompt("informe um número inteiro positivo");
+for(i = x; i >= 0; i--){
+    console.log(i);
+}
